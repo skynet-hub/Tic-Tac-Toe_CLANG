248c039c2227a64720fd8f81667971a3e03b2aee
Magobo <malesaojhb024@student.wethinkcode.co.za>
2025-10-11 23:44:07 +0200

    Introduce game loop and clean up starter code

--- game.c
+++ game.c
@@ -1,9 +1,19 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "time.h"
 
+
+/*
 const int EMPTY = 0;
 const int NOUGHTS = 1;
 const int CROSSES = 2;
 const int BORDER = 3;
+
+*/
+
+enum {EMPTY, NOUGHTS, CROSSES, BORDER};
+enum {HUMANWIN, COMPWIN, DRAW};
 
 const int convertTo25[] = {
     6,7,8,
@@ -37,12 +47,36 @@ void printBoard(const int *board){
     printf("\n");
 }
 
-int main(){
+void Game(){
+
+    int gameOver = 0;
+    int side = NOUGHTS;
+    int lastMoveMade = 0;
     int board[25];
 
     initializeBoard(&board[0]);
     printBoard(&board[0]);
 
+    while (!gameOver){
+        if (side==NOUGHTS){
+           //get move from human, make move on the board, change sides
+        } else {
+            //get move from computer, make the move on the board, change sides
+            printBoard(&board[0]);
+        }
+
+        //if three in a row exists then game over
+
+        //if no more moves then draw
+    }
+
+
+}
+
+int main(){
+
+    srand(time(NULL));
+    Game();
     return 0;
 }
 
